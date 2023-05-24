d5db112486a40527d9b0510be34f83d37b6fc72d
Samuele Tatini <44943668+samueletatini@users.noreply.github.com>
2023-05-24 11:31:52 +0200

    Somma tra due numeri

--- Prova.ts
+++ Prova.ts
@@ -1,1 +1,14 @@
-console.log("Hello, world!");
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Inserisci il primo numero: ', (num1) => {
+  rl.question('Inserisci il secondo numero: ', (num2) => {
+    const sum = Number(num1) + Number(num2);
+    console.log('La somma è: ' + sum);
+    rl.close();
+  });
+});
--- README.md
+++ README.md
@@ -5,8 +5,8 @@
 Hello world!
 
 Elenco numerato:
-1. Ciao 1
-2. Ciao 2
-3. Ciao 3
-4. Ciao 4
-5. Ciao 5
+    1. Ciao 1
+    2. Ciao 2
+    3. Ciao 3
+    4. Ciao 4
+    5. Ciao 5
